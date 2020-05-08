dd8729b43b20ffcb6b72c8af3ac9ad4d40a26c85
Codrin iftimie <iceboxic@gmail.com>
2020-05-08 21:37:32 +0300

    Fixes

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46674fba1a69ae2ed5dd98f314a01d610d4b1cddf442788cbac29d928678b294
-size 216
+oid sha256:854c8fc3340ad124cf03b52254a6b0c7c6dbe5f9e12cf557c1f9467f18dc8eac
+size 224
--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:380a3c55180ccfdc57a8614c5cb66258c7ef0aec5a5722390a824911431df594
-size 193
+oid sha256:afd75ab0a22b091672e443520aec01ed3514fd40f92cc7431208c6bfcafcddbd
+size 162
