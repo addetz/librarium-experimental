804a61ec795b1269ba179ab25f4e3bacf44316e8
Codrin iftimie <iceboxic@gmail.com>
2020-05-08 21:37:32 +0300

    Fixes

--- packages/docs/src/App.js
+++ packages/docs/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1c92ed26c2b3e2416592e1068917062a63cb077ae1e1d2308eb1a54a9a636de
-size 137
+oid sha256:c3ca962d06499b0d0bb145908849893aa6424b019876a8abcad77c188002d648
+size 106
