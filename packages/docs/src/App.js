cd4035a2a74e3fdcdaea60968fe1fbf2da082ff2
Codrin iftimie <iceboxic@gmail.com>
2020-05-08 19:07:13 +0300

    Build shared separately

--- packages/docs/src/App.js
+++ packages/docs/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dce09c94a58ec1b4f6352bd72d777b2e9889e2ae39170516c65457ed012c09d6
-size 107
+oid sha256:e1c92ed26c2b3e2416592e1068917062a63cb077ae1e1d2308eb1a54a9a636de
+size 137
