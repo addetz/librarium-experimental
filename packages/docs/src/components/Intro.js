4085f7678e4afca8b2fe8fa742e81ca9d75ccea8
Codrin iftimie <iceboxic@gmail.com>
2020-05-11 19:23:15 +0300

    Add homepage styles

--- packages/docs/src/components/Intro.js
+++ packages/docs/src/components/Intro.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfa02b15aba1b458cba8a66a41d1e2998104a5830f1b092f7ec6acf458d436a3
-size 281
+oid sha256:51745028d1efaf52c9f436d0363b595facbd2709f84fbe405056acd1684316bb
+size 326
