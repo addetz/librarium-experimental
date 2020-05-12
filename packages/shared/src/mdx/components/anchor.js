d48f35fc4966838fa5f76b442b5a669158aafb9f
Codrin iftimie <iceboxic@gmail.com>
2020-05-12 19:22:15 +0300

    Review

--- packages/shared/src/mdx/components/anchor.js
+++ packages/shared/src/mdx/components/anchor.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffb9b61361add5af2d5840a681d159edb4279b7b00537327c0ce41c1e47ef570
-size 324
+oid sha256:c940745e24c053eb61472576333872f06d00c850ac7ee9774276fd5f0bc1f1fd
+size 282
