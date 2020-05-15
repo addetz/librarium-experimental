dd8137376dcf2844f996678854d78680b4cdc6b5
Codrin iftimie <iceboxic@gmail.com>
2020-05-15 18:48:17 +0300

    Add todo

--- packages/glossary/src/layouts/docs.js
+++ packages/glossary/src/layouts/docs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c98a48e0d4b6599a0cfff510877dcb4f9464c24f3c695cdd08944d9876ade7a4
-size 2588
+oid sha256:53e3310a0da286385e7adf17267e967fe6664a7b513aa48f38c66ca4368d8d6b
+size 2667
