5dca05ecd43a21fef8cc54d0ff7c2d3b615e7ad9
ABILASH K B <64770682+abhikb2005@users.noreply.github.com>
2020-05-22 15:19:36 +0530

    Removed console.log(node)

--- packages/docs/gatsby-node.js
+++ packages/docs/gatsby-node.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ff1e94c0efb4b05b921a6d060712678bba3dd6b7faa4fa4f3c10c86024d30d3
-size 3350
+oid sha256:90bab36f6c020a6bba7a9a4726311838a722cba0626a042419eeb32e554452a8
+size 3311
