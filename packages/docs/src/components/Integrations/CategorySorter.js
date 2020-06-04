fdf470b0ec5ec442d27924305f692e234a28553b
Mihail Iftodi <mihail@spectrocloud.com>
2020-06-04 11:06:31 +0300

    implemented integration search

---
+++ packages/docs/src/components/Integrations/CategorySorter.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98338aab6a5efddbf57570af3d15877876cecbbd922a18cb48eac55423717103
+size 1005
--- packages/docs/src/components/Integrations/index.js
+++ packages/docs/src/components/Integrations/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3ac11a18584cf4759565f2cf08ce877915562d52217ffa749e91df462a01789
-size 3509
+oid sha256:2c0b2e828fe14510c59816979bedfa75f590f2246ad83943bcbd38295fb66b4d
+size 3955
