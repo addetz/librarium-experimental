a24ed66a226c05025d9b41a12dcc5d496c4cd337
Codrin iftimie <iceboxic@gmail.com>
2020-06-04 19:16:55 +0300

    Add category selector back

---
+++ packages/docs/src/components/Integrations/CategorySelector.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a5871ea1e191a56b4e348aff0df3e0990072a10382202b9089629f9cb9c4c69c
+size 985
--- packages/docs/src/components/Integrations/index.js
+++ packages/docs/src/components/Integrations/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:119e984e0acdf7142cdb1dbe604f2cd03c698b6a5751071fe6315e1120812624
-size 2639
+oid sha256:16b687f713c222d43d57dd1ac8ee3c26ee735dd0e82626e9c72f03256b6c8a0f
+size 3266
