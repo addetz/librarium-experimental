6e86d6a1302c3e751cd9f438a73b7093abef2519
Saad Malik <saad@spectrocloud.com>
2020-06-11 10:21:40 -0700

    Delete v2 (#26)

--- packages/api/content/v2/00-clusterprofile.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dfc929cac367df3c4daef37cac915605cd198878f50d1eabff36005daca11905
-size 150
--- packages/api/src/templates/docs.js
+++ packages/api/src/templates/docs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:925d038bd117e694b74949fea1926eead5d98f4a255ef5959a2c9f55c29a2673
-size 3527
+oid sha256:1b5d46dabdd1064af9217e6f9956082698da6f36abc52bdf639accd8bb439102
+size 3477
