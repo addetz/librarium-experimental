8679b58c879b27a0cc70614bdcfe46ace5394369
Ionita Catalina <catalina.ionita.m@gmail.com>
2020-06-11 18:29:11 +0300

    Content updates for docs (#25)
    * Content updates for docs
    
    * requested changes

--- packages/shared/src/components/styles/PageNavigationButtons.js
+++ packages/shared/src/components/styles/PageNavigationButtons.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b59534f966a02747be3e9007bcb8b42356b90a4eff835c7ebae05b7001ab003a
+oid sha256:8974f8cfd2bc66f5c4be9395938de549fd2b4a041d3fc732ff23e7863111d587
 size 2399
