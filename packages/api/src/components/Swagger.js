a604a53c3729fe953163eac5396f8d2c6d005b63
Ionita Catalina <catalina.ionita.m@gmail.com>
2020-06-12 16:28:43 +0300

    Api small touches (#28)
    * Small fixes
    
    * fix

--- packages/api/src/components/Swagger.js
+++ packages/api/src/components/Swagger.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adc75820840d7be995744debdc8fa63c81e0279ebe121dea6b8e4f2b56fbc7ab
-size 3899
+oid sha256:6a48297218d9d83b5d529c5c910156da522cc762ad78316a351a144e610aca46
+size 5134
