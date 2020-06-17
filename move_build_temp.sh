b5e5039f2e56ac0b79a30cf7771c403ec3db0d02
Codrin Iftimie <codrin@spectrocloud.com>
2020-06-17 18:52:08 +0300

    Fixed move_build (#31)

--- move_build_temp.sh
+++ move_build_temp.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd11c2a5875487268860b73a3e64e039e5c7c478f5fa59b0134f3e76df639cda
-size 139
+oid sha256:c81585f87b3b1fbc7bfe5af5d83be53d559743a560ae8162f75b86dfc884e372
+size 62
