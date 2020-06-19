e08a79963c3be68651e0e296a3216e88d99b5651
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2020-06-19 18:18:42 +0300

    Mihai/ga fixes (#44)
    * fixed moving logo
    
    * should fix icons getting big
    
    * 404 page
    
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>

--- packages/shared/src/layouts/Default/sidebar/index.js
+++ packages/shared/src/layouts/Default/sidebar/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dbf8cbcb6ab5cbe27196b665fa56d65a2c8faa84beada8a2d56b548a2179522
-size 5383
+oid sha256:50f7398494cb3a0cd19cce67050ac6465db23e635eda662b4828572d1cc46d91
+size 5484
---
+++ packages/website/src/pages/404.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb5dc882411dc0122bb9ac0a53171b88335bc84a3e82def2ebff613f7486db7a
+size 1320
