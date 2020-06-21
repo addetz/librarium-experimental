096bf70915c4f55dc559e243068c5a049d69c284
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2020-06-21 10:59:25 +0300

    Mihai/fixes3 (#56)
    * integrations images auto-scale
    
    * remove capitalization from table headers
    
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>

--- packages/shared/src/components/styles/Docs.js
+++ packages/shared/src/components/styles/Docs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ea8518a6844a863d18dccbc0f36ccfa65c0eb4e68841c9b7565358c0b203a15
-size 2805
+oid sha256:3dc360d17846347c7c0a0e033e57f16a1e359b77489c38a2e7721db1cd966ef7
+size 2771
