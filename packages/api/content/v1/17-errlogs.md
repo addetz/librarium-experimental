4384a92eac45cab42725a9bba76d22eb4736f979
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2020-06-22 21:04:45 +0300

    remove system/private ep, sort titles, remove ep paramterers as method (#76)
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>

--- packages/api/content/v1/17-errlogs.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aced5bc0766768100607831caef3933cded21431aeccb2fc1428c4bad2f2f62b
-size 176
--- packages/api/src/components/ApiSidebar.js
+++ packages/api/src/components/ApiSidebar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d5805ff7aeab714d8fe22816a57a5afe206bdd0eb24f1d2e78fd743a781eeba
-size 1774
+oid sha256:3889bb4cfb6cb9af78bd37bf9cc02b172a138789e1d373c3a62ef938e12dda75
+size 2009
