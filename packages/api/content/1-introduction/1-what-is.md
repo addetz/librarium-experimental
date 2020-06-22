33bb3c5ad4136207a662b1236e9b2d058f7f037c
Saad Malik <saad@spectrocloud.com>
2020-06-22 16:50:29 -0700

    Change home and remove api intro (#89)
    * Change home and remove api intro
    
    * Stylish home

--- packages/api/content/1-introduction/1-what-is.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96e3317feba681ccec05ac38e44236bf95bcd317b5961d12c626d06b4396aee9
-size 2590
--- packages/website/gatsby-node.js
+++ packages/website/gatsby-node.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39e2d606f3c37b1258f741a1969c8dff896dc8cff46bcd3ab9d6483323ed5495
-size 5289
+oid sha256:36479f838dfe2b363bd4f5952778e44ccf0b104fc85db857ba98dc25a7886ccc
+size 5342
