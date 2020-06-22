ae46103535c2acf3ac0b87914b096d38b9d63f6a
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2020-06-22 14:04:25 +0300

    now scrolling stops right at the title (#66)
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>

--- packages/shared/src/components/styles/Docs.js
+++ packages/shared/src/components/styles/Docs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dc360d17846347c7c0a0e033e57f16a1e359b77489c38a2e7721db1cd966ef7
-size 2771
+oid sha256:1b7a67119a1dda478a3a8d7879457d5f34082f1c1b39de6f7cc90a9205b23145
+size 2907
