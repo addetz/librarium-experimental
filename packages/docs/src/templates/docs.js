cb5d4008efeaa875a6114d26a10690a47bc66fc5
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2020-06-22 11:14:39 +0300

    added hideToCSidebar (#64)
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>

--- packages/docs/src/templates/docs.js
+++ packages/docs/src/templates/docs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02d06691ce558233af5a0752048596076fd2eff094132b327e1536b77fffd92c
-size 1645
+oid sha256:b7d96d8d0a8810a5ea80524ee20b6805f2288a858ebc0f5931e8afcc9554541b
+size 1668
