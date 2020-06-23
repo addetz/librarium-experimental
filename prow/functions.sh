516842d2d80757f1852d77b6c0f799b5ca748351
Zulfi <zulfi@spectrocloud.com>
2020-06-23 14:26:23 +0530

    Algolia params (#94)
    * Run with 8G mem
    
    * Run with 8G mem
    
    * Add Algolia search parameters to build

--- prow/functions.sh
+++ prow/functions.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc6eccb77bcfeb548b74a9f35d1204acbeebab6ae7a8de8b68db9571e72de58f
-size 693
+oid sha256:7b4f5263bf4d2fb414863dc74e7f1d86c8e46a1aaa25c9252fdbaf24c2862328
+size 834
