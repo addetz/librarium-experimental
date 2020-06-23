d3c94633885b7f7a8b762dbb0f4c8501162fbff7
Zulfi <zulfi@spectrocloud.com>
2020-06-23 09:43:31 +0530

    Run with 8G mem (#90)
    * Run with 8G mem
    
    * Run with 8G mem

--- prow/functions.sh
+++ prow/functions.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:389ed25d790def5d1c615ed44b18ff03899770f760a46a9e66e0f9275ace4eb9
-size 680
+oid sha256:dc6eccb77bcfeb548b74a9f35d1204acbeebab6ae7a8de8b68db9571e72de58f
+size 693
