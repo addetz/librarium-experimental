c21bfc6c4dc94b9f840c262c2bf0de6973ca589d
Codrin Iftimie <codrin@spectrocloud.com>
2020-06-24 19:13:36 +0300

    Codrin/fixes (#106)
    * Light theme and DOC-47
    
    * Fixes for the title selectio

--- packages/api/src/components/Swagger.js
+++ packages/api/src/components/Swagger.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7653708a9b5e6dab38b3ae734831a628b0652b4c06d249a730b5ed20cefe8386
-size 5362
+oid sha256:186231bb2787d8c01c57368845bf3f87019d953ec1be27ca37847309fff7a714
+size 5365
--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02b6c866fe3a12522d7dd36bc83480aaf38ae36d65a7bfe34b8a14de7659ff81
-size 3530
+oid sha256:42ded7a64230ca6cfb3f67576c0df8d31fdbc7d775c5ca7548f9dff49943af1a
+size 3570
