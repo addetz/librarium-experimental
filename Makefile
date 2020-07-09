ad2f22ef7aa486538332873a3662b78dadc4dfe7
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2020-07-09 16:56:57 +0300

    Copy to clipboard (#115)
    * added functionality to copy to clipboard
    
    * deleted unnecessary component
    
    * Alternative
    
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>
    Co-authored-by: Codrin iftimie <iceboxic@gmail.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb28e24918c2a4e04900a7cfd4568463fedb40df4349a3ba67cb415a3797b0c9
-size 270
+oid sha256:b30b3d2c8e20fbc8385e3fd29092940a119dea36b30ccd6394f6b39ce60783f9
+size 293
--- packages/shared/src/components/styles/Docs.js
+++ packages/shared/src/components/styles/Docs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0433a950e7adbe93ef9161979477cb0975a20938f74081e6280ec7532831c4a
-size 2929
+oid sha256:29c5a9e5b90dfe7e9fea8ad637481c7a9d7af73cd9c2fe31d26b3addd1529340
+size 2932
