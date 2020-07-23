81fab2fe9397880d01789f2477dc91cd671e8057
ABILASH K B <64770682+abhikb2005@users.noreply.github.com>
2020-07-23 18:27:23 +0530

    Homepage content (#129)
    * homepage header and Quick startup section
    
    * correct key
    
    * clouds section
    
    * Mocked Docs updates section
    
    * fixed splice startIndex
    
    * refactor
    
    * Updates to Custom Registry and CLI references.
    
    * Add a way for a page to query aditional data
    
    * Home Page Content First Draft.
    
    * Added tabs to CSI pack and CLI. Typo correction in Release Notes and Intro Button. Added a section in the homepage.
    
    * Update 2-spectro-cli-reference.md
    
    * Fix width issue
    
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>
    Co-authored-by: Codrin iftimie <iceboxic@gmail.com>

--- packages/docs/content/09-registries-and-packs/2-spectro-cli-reference.md
+++ packages/docs/content/09-registries-and-packs/2-spectro-cli-reference.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e119aa1340d4522775864aedc6b0331c523619fab38125fc6a10689772a1d63
-size 5576
+oid sha256:0a8467da32893014e8d9db52bb3d45ca2300a9040daaa0a7845df259d9431cd2
+size 6609
--- packages/shared/src/components/common/QuickSetup.js
+++ packages/shared/src/components/common/QuickSetup.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6329277a3f1b29d35452058fe88ec287d791f6437bca027c64253deadd60c45f
-size 1805
+oid sha256:705acff7936d5eaf41d75644a2772aaa31b6b309e532cb0401f204e6346462c3
+size 1820
