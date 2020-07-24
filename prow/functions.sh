f3746d62200dab67bc4468d0c086f7bbf0d7da4a
Zulfi <zulfi@spectrocloud.com>
2020-07-25 02:22:10 +0530

    Rel build (#131)
    * Run with 8G mem
    
    * Run with 8G mem
    
    * Add Algolia search parameters to build
    
    * Add release build

--- prow/functions.sh
+++ prow/functions.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b4f5263bf4d2fb414863dc74e7f1d86c8e46a1aaa25c9252fdbaf24c2862328
-size 834
+oid sha256:14fd69b5c5846eba82debd19d6b8786f5f1b9ee7e270e531c318c6f691f09d6e
+size 1261
