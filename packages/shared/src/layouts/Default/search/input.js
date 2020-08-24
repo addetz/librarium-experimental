1ec60253c82ad528c393736d6373fbe4355183e2
Codrin Iftimie <codrin@spectrocloud.com>
2020-08-24 16:03:02 +0300

    Improve SEO (#150)

--- packages/shared/src/layouts/Default/search/input.js
+++ packages/shared/src/layouts/Default/search/input.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a37d88411a4fc43e083eca588de1a09335320f8e9440bd0279010a85a765d06
-size 1220
+oid sha256:4842354866da1e58e49ddb8d9a173257ef1d1795e703b3e4fdb3884b74af1eaa
+size 1214
