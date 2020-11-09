1b67a9ae8d8edd2d98c2a1ffa16fd06ea4b67600
Iftodi Mihail <33319632+iftodimihail@users.noreply.github.com>
2020-11-09 13:36:34 +0200

    Header permalink (#244)
    * permalink to h1 and h2
    
    * tabs permalink
    
    * review
    
    * tabs permalink
    
    * small fix
    
    * review
    
    Co-authored-by: Mihail Iftodi <mihail@spectrocloud.com>

--- packages/shared/src/mdx/components/index.js
+++ packages/shared/src/mdx/components/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9844811d784c354442629776ce3e3b8ae83bd47c9e7d15f68447cdc3ba146a77
-size 2393
+oid sha256:e2b67b39cab97711cfe74d4f0ee1888e627a6d3f8bdf1075921004f7e723b8df
+size 3662
