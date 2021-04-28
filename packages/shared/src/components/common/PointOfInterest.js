25fd6f2f02c6fd9c52179806a04958e4e398fe12
Ionita Catalina <catalina.ionita.m@gmail.com>
2021-04-28 15:48:08 +0300

    fixes (#336)

--- packages/shared/src/components/common/PointOfInterest.js
+++ packages/shared/src/components/common/PointOfInterest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aeb14a6d67090283ef1cb472b564b78c26b3f27b0ad5040c2455834b627ff760
-size 2203
+oid sha256:7ae7bce9a951682b36d1c798a06a0e8d540e699d4269cdb0dc9826382a06aad2
+size 2225
