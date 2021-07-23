937241db265e8fb24917498900d7e2386dfd873c
roshanirathi <42164609+roshanirathi@users.noreply.github.com>
2021-07-23 16:12:22 +0530

    add auto-reconf (#412)
    * add auto-reconf
    
    * install automake instead of autoconf
    
    * install autoconf with automake
    
    * install libtool and pkg-config
    
    * pkg-config not found
    
    * install pkgconf
    
    * removed commented lines

--- prow/functions.sh
+++ prow/functions.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14fd69b5c5846eba82debd19d6b8786f5f1b9ee7e270e531c318c6f691f09d6e
-size 1261
+oid sha256:950bec27809050ee2f58d4e89ae9c9232858cb1235839330ddf486d8806b26ff
+size 1329
