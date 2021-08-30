0f4fcc97a6b9aa7008833d1b6df81433b502396b
Ionita Catalina <catalina.ionita.m@gmail.com>
2021-08-30 09:12:36 +0300

    centered search (#442)

--- packages/shared/src/layouts/Default/search/index.js
+++ packages/shared/src/layouts/Default/search/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57e2c182bda80e6ec85d9d140e1a389dcc4cc3f3af36e3336e7776fac746aeb3
-size 4593
+oid sha256:5cdb9436d3107ed29684ba6b6cd6e8a3bdd35c5940e6afb6d894acb692de6c09
+size 4835
