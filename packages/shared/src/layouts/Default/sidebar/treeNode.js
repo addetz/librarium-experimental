28054c7fa4d069cc459e9ed8b3b2c5e5736340b6
Codrin Iftimie <codrin@spectrocloud.com>
2021-09-14 17:44:29 +0300

    Fix menu levels (#451)

--- packages/shared/src/layouts/Default/sidebar/treeNode.js
+++ packages/shared/src/layouts/Default/sidebar/treeNode.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae584267f66f116f31e19426d40cb0a0fe479c5058a9326ffca5744b0c119794
-size 3058
+oid sha256:139a21ae3fcea7b339d3db38974b500b45f992accccd0e145a570734da6994fa
+size 3109
