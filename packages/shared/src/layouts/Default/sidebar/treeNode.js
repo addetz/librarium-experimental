3c0be74aae90005194c234dc812dc1b5dd641983
Ionita Catalina <catalina.ionita.m@gmail.com>
2021-09-15 10:52:29 +0300

    small fix (#452)

--- packages/shared/src/layouts/Default/sidebar/treeNode.js
+++ packages/shared/src/layouts/Default/sidebar/treeNode.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:139a21ae3fcea7b339d3db38974b500b45f992accccd0e145a570734da6994fa
-size 3109
+oid sha256:2bcffadc041ce477f8b250e113c5f9f9ce11d1e6ae671c7c17ca6da721914f2e
+size 3108
