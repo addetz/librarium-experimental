a3fcbabff5e742b825a327d3d0b0c4f9f5eeccb0
Ionita Catalina <catalina.ionita.m@gmail.com>
2021-11-08 13:37:04 +0200

    added support for video (#496)
    Co-authored-by: Ionita <catalina@spectrocloud.com>

--- packages/shared/src/mdx/components/index.js
+++ packages/shared/src/mdx/components/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3557e2b34edee329aa27a8f192e6f29a644201d603c33d8424be82809441d1ea
-size 3662
+oid sha256:3647af1cacf473cab56d74ecdd924b3fbaa74f8fd2169c8e3a76ab5b10600504
+size 4022
