ffe6422e369b338359f647e28aa88dfbf00b3755
Ionita Catalina <catalina.ionita.m@gmail.com>
2021-11-10 17:29:54 +0200

    added plugin support for videos (#518)
    Co-authored-by: Ionita <catalina@spectrocloud.com>

--- packages/shared/src/mdx/components/index.js
+++ packages/shared/src/mdx/components/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3647af1cacf473cab56d74ecdd924b3fbaa74f8fd2169c8e3a76ab5b10600504
-size 4022
+oid sha256:550fa0015a0014f4af0d3fd22c7b7f2220934582ecc927967d25ac95393cd289
+size 3710
