a29d3f19dcb873e8367a35ea432497aa87403b3e
Zulfi <zulfilee@gmail.com>
2022-04-25 16:34:11 +0530

    Change Algolia keys for Release Build (#676)
    Co-authored-by: Zulfi Ahamed <zulfiahamed@Zulfis-Mac-mini.local>

--- prow/functions.sh
+++ prow/functions.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:950bec27809050ee2f58d4e89ae9c9232858cb1235839330ddf486d8806b26ff
-size 1329
+oid sha256:d302f15bfb7f2b20d6db6ab24f014cdf763255689a74f1278bfdb396fb1a7fa7
+size 1640
