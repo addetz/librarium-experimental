b55e94d68c592e5bebba76d8dd955be899a88c01
DianaTerchea <44743846+DianaTerchea@users.noreply.github.com>
2022-05-03 21:04:19 +0300

    BET-4028/ API doc link to Git edit not accessible (#679)

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3883e068cb4bdbc4e9f6f46b0d8571c83cd42e605fdc86d03d25397e4930f193
-size 1255
+oid sha256:f0e05f1fd98250fee6ac7d45d99333802a08bf63c47b070980eb436d1f53fdbb
+size 1340
--- src/templates/api.js
+++ src/templates/api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8dfa3c6b2ff3b076d13571e64f033625dbefcb8882f6ad52b5b3c513eb0dc696
-size 7297
+oid sha256:843a0cf7882de6597b6a7ae1b195ff8949bc2a48cd47a90695c9af045925e87d
+size 7294
