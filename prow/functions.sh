45d75f559af47d54edd484c8c672d901801adab0
Codrin Iftimie <iceboxic@gmail.com>
2022-05-27 21:21:17 +0300

    Codrin/link fixes (#714)
    * Fix link styling
    
    * Add ENV variables

--- prow/functions.sh
+++ prow/functions.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d302f15bfb7f2b20d6db6ab24f014cdf763255689a74f1278bfdb396fb1a7fa7
-size 1640
+oid sha256:a4e5097669472a7f3a9113b61b3098caa26301ad1c51b72223c6842482d1f28e
+size 1733
