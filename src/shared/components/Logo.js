5fc5badb98af5dc52c1d0d0be3d37bdeebec742a
Mircea Bozianu <mirceabozianu@gmail.com>
2022-10-17 12:30:17 +0300

    Updated Logos and Favicon

--- src/shared/components/Logo.js
+++ src/shared/components/Logo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:778754a2e869799f305083054489bc9bf8f8b1f70493f318b2a840d49720e6c9
-size 15778
+oid sha256:73adad3d04b9d2d7452a58011bfc8d4c72cd3ef2e54ff569cdb3b9cbea245647
+size 29492
