5c38d8be4dfd50a4760166c90b2d8baa786b2a28
Karl Cardenas <karl@spectrocloud.com>
2022-11-03 20:03:36 -0700

    ci: changing order of test script

--- prow/presubmit.sh
+++ prow/presubmit.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60d6d865024526fa76dafc5cb0a6b325149c9e47f5a222c16411e0b2a4428633
-size 206
+oid sha256:c7d29894dba9afa2782e7a8d90c4197fb18d9cab95ceaf866533c52e7e30b5cc
+size 208
