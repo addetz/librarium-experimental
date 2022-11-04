6936214173d59db76f127b28cb44347ce71ff427
Karl Cardenas <karl@spectrocloud.com>
2022-11-03 20:21:04 -0700

    ci: changing order of test script-2

--- prow/functions.sh
+++ prow/functions.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db19913dba9350b2bb9ce9c3101571dbbac65e9b969cf5f1fe3a0ee4a47f195b
-size 2301
+oid sha256:ce0ca981f899f28e165224db35cf079453eec95f2510b55f010edf2aae9eb467
+size 2221
