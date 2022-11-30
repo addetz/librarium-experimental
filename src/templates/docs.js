ba8718a9eb2c61eb092d77da8a807b5a220519ec
Ionita Catalina <catalina.ionita.m@gmail.com>
2022-11-30 16:24:32 +0200

    verto markup schemas (#650)
    Co-authored-by: Catalina I <catalina@spectrocloud.com>
    Co-authored-by: Karl Cardenas <karl@spectrocloud.com>

--- src/templates/docs.js
+++ src/templates/docs.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f642dd58128df8f4d548edb22fc9bee29ab58b983dba8a3a9457d19b7f18421f
-size 1614
+oid sha256:6e5c70c0b62ccb5e7f868337a6e30cef074d7d804fbf332a11d0144f18438160
+size 1743
