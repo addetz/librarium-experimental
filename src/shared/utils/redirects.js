d506fa464c27e428a2520e93d4dc5d5fd79062da
Karl Cardenas <karl@spectrocloud.com>
2022-12-02 10:48:18 -0700

    chore: fix redirect (#1018)

--- src/shared/utils/redirects.js
+++ src/shared/utils/redirects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cab79aefca64c95110a8970395465eda2cb238063604205f451aa768d0150c48
-size 1407
+oid sha256:9fb4d172aa6269cfb92095181b28b1925d744d1ed9345f0f6ecdd3087e4c87fc
+size 1459
