0fac6a6c8b28024e0b4c2119c5dc3906d18eb98b
Karl Cardenas <karl@spectrocloud.com>
2022-12-14 11:40:10 -0700

    docs: app mode and cluster mode explained

--- src/shared/utils/redirects.js
+++ src/shared/utils/redirects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baf74fcf471ea50a6c50acd3e29c573a338ba466be6e89d032246170c814cb54
-size 2164
+oid sha256:1ee05eda3e3cfc7782104b2f6d619a427d5aa2ebc0f0306bd553283a2b40e4e7
+size 2293
