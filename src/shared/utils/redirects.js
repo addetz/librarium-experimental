ea17538679e436d91b48254d2253f61adeedfb8e
resmaa <49595451+resmaa@users.noreply.github.com>
2023-01-04 19:53:04 +0530

    Update redirects.js

--- src/shared/utils/redirects.js
+++ src/shared/utils/redirects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dd1cfe1e0236d6a98947098bb2b8427b0cf318d96f7955b14c62bd065a64e30
-size 3620
+oid sha256:219652fc3724df06acf7dd1d3bd5680ee31ac7cc9e2c5dca4e55a841612cb275
+size 3475
