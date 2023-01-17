b8bac6e5141df74b7343ebf5105933325315e64e
Karl Cardenas <karl@spectrocloud.com>
2023-01-17 14:44:43 -0700

    docs: added more content

---
+++ content/docs/04-clusters/06-cluster-management/20-reverse-proxy-dashboard.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5b53d4549adebf2d5dea42dcd7b894dd1443a2cc5f42e90a64ad83341658e566
+size 4679
--- src/shared/utils/redirects.js
+++ src/shared/utils/redirects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:219652fc3724df06acf7dd1d3bd5680ee31ac7cc9e2c5dca4e55a841612cb275
-size 3475
+oid sha256:8fd42988d67263aaec932efb1f795ab1629a0006e459bc7f0899c239e90ca8f7
+size 3666
