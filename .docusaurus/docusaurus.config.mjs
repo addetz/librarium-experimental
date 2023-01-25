06fa49d4fad9409eb966ab564b29df0efbb88aa5
Karl Cardenas <karl@spectrocloud.com>
2023-01-25 15:57:11 -0700

    docs: prepared kubernetes dashboard for future refactor

--- .docusaurus/docusaurus.config.mjs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3c4d31bc4c794483ea0854a74d096458be9715f94cd79fc7a2d48814190143d1
-size 7725
--- .docusaurus/registry.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8d1192372ba97d5accef183815be16709b68db2bce74894b232f669917a312a6
-size 15309
