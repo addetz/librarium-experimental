4ed5c3df4b9f79910a63fc6247de9e9dd3ed2305
Karl Cardenas <karl@spectrocloud.com>
2023-02-13 07:32:50 -0700

    docs: fixed missing image name DOC-722

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c5fe7a932a53eeb8f7bc0bee6534e05ac5a4c1b7ccd98557282d2ef5eadf0af
-size 4951
+oid sha256:e61e200f317c8ff265c8cb829eef8a3aac06744eac62666b0e9371a77eb3841d
+size 4958
