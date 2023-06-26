2e99c11bdeba4bce3081c17bf14f6cb452a3c6b2
Karl Cardenas <karl@spectrocloud.com>
2023-06-26 09:42:06 -0700

    chore: updated ssr code for full story (#1381)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:666ce3efb9e8dbb9da9a46d693ca862fd4a1cc07dbabca9db1e49c9d8b34f989
-size 2048
+oid sha256:8a70ce81bba70b3a6a9ccffe6f3c29e5c53831d4760232a417e201eef0e2c06a
+size 2017
