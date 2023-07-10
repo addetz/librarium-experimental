ee6b61b6059e8173382fa1ed2ce5d87d68069be4
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-07-10 11:24:46 -0400

    reduce margin between text and logo (#1408)

--- src/shared/components/common/Technologies/index.js
+++ src/shared/components/common/Technologies/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b97fa3d88f82dc8d4aa0cf68e4c3e25138010f5eda3014653410df5da06653fc
-size 2988
+oid sha256:88ce93dc9bd7bf14cfc8214ec3cc2fbb883cd3969b27ebe8fb77f54634c61cc2
+size 2986
