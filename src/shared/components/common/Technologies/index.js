9c5caf3bcdff5bf610f9ff1e225aa51be4431e5d
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-07-12 18:23:40 -0400

    Add Multiple category selector  (#1415)
    * iterate category and add to set
    
    * remove demonstration changes
    
    * adding sorting capabilities

--- src/shared/components/common/Technologies/index.js
+++ src/shared/components/common/Technologies/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88ce93dc9bd7bf14cfc8214ec3cc2fbb883cd3969b27ebe8fb77f54634c61cc2
-size 2986
+oid sha256:13c3123b2e6f82f9df628ce104b28cd5f6c1d22741113409ef5572cceb45f269
+size 3131
