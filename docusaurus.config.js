ad654de09d33ca5909f983673a3556c1979ccb15
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-09-02 22:57:04 -0400

    Change edit URL (#1513)
    * change edit url and remove autogenerated sidebar.js file
    
    * add sidebar.js to gitignore

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30f87b328550e195af4c6895f7407365c73806e71d7a04d6db7a7ef184f70f47
-size 9763
+oid sha256:ae3de530e894bf0276611f73bc2199c2082d37eda1e7c6d5187ee876ccb72102
+size 9766
