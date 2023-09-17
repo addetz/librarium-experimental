14c6e03fb62449626f9fafd06bfecaba99b9cf16
Karl Cardenas <karl@spectrocloud.com>
2023-09-16 18:39:39 -0700

    ci: added version prefix in label

--- scripts/update_docusarus_config.js
+++ scripts/update_docusarus_config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6549418da97f5799c2588be9cc20b4e2da35ace3350d99f750f9df62f6ddb313
-size 3697
+oid sha256:5433d3f39ba14744ea85878d4626f0796f4a9d37cac3d285e5a4c5fceb5a68cf
+size 3703
