18a401c20ed775db3d2a7091f2276fffe91911b1
Karl Cardenas <karl@spectrocloud.com>
2023-09-19 20:09:55 -0700

    ci: adding missing commit (#1573)
    * docs: updated README and release concurrency
    
    * ci: update version branch PR CI
    
    * Apply suggestions from code review
    
    Co-authored-by: Rita Watson <117382432+ritawatson@users.noreply.github.com>
    
    * docs: updated README with more examples
    
    * docs: updated downloads file
    
    * docs: updated checksum
    
    * ci: fix npm issue
    
    * ci: eslint fix
    
    ---------
    
    Co-authored-by: Rita Watson <117382432+ritawatson@users.noreply.github.com>

--- docs/docs-content/spectro-downloads.md
+++ docs/docs-content/spectro-downloads.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cade36f895381265e76fccea8f352f1a0bbb2efe3a5f25ace12b2b34a5c83cd7
+oid sha256:03f0a6c097d8912974d9f03b78a8df0f064bdabb782c7cda359e90f4598ad1da
 size 4225
--- src/components/MendableAIWidget/MendableAIWidget.tsx
+++ src/components/MendableAIWidget/MendableAIWidget.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eb233aa2038997c44cfd46450ba50e51b7775743e85998c0fb1fd9150607128
-size 1360
+oid sha256:9dfd2fdc7803b9a79ee09001d6113574b3ceea87584e4ea48d515709cfccbbe7
+size 1424
