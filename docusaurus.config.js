a7136e240e3a599eaa30236313b4e5ad6d7ab908
Karl Cardenas <karl@spectrocloud.com>
2023-10-03 09:03:36 -0700

    chore: updated privacy settings experience (#1635)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29f11e030ec31d0718797518a6a5bf731536e7fe681933d41683f19bcd1fc47c
-size 10518
+oid sha256:e3e162d8dc7f26b4ab3331ee3ba122fb95fc9543dffebcf8c03395399c1e34e0
+size 12018
