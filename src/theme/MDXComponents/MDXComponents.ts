b7d39618afa387b010273375a437a2e5317018cc
Karl Cardenas <karl@spectrocloud.com>
2023-10-20 07:34:58 -0700

    chore: added toc to the global scope (#1685)

--- src/theme/MDXComponents/MDXComponents.ts
+++ src/theme/MDXComponents/MDXComponents.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e6902b6c4e7d49e41b7e04987de5a996f3cdb8d8727e051c9b86e2ee2e774d9
-size 765
+oid sha256:373aa1873f7dc7909b87dc22e4cff952f0c63b390a481b621970e3c36d4711ab
+size 820
