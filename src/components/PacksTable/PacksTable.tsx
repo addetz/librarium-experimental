81b124f59efdf42c77d74ae1f9946622e7c84e00
Karl Cardenas <karl@spectrocloud.com>
2023-11-01 13:42:08 -0700

    chore: added correct field to display (#1738)

--- src/components/PacksTable/PacksTable.tsx
+++ src/components/PacksTable/PacksTable.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac330fe88d3a62cd4c8ab91129dda0bd2b726fc19faef6003d252ad3477af3bd
-size 5341
+oid sha256:02edb37f38a3b4ca50f1364190ea4c25101a025b79a80cc007590467b62ae404
+size 5357
