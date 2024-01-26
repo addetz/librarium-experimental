8ddd5ffea4dfc56f207d2a999e73f0e4c6ff8798
Karl Cardenas <karl@spectrocloud.com>
2024-01-26 12:15:45 -0700

    docs: removed rouge page (#2129)

--- src/components/IconMapper/dynamicFontAwesomeImports.js
+++ src/components/IconMapper/dynamicFontAwesomeImports.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:959d01a22ba9453c4a288869a9b617e08d3e0cd356c5ea878ad6ca48c482a221
-size 1791
+oid sha256:accb5591c2ce2909d4a8c2c254901ca3806bade0478e432bbd001b18f376776c
+size 1694
