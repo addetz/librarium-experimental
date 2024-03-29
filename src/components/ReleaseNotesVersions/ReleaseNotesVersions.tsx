490784196cf9deaba80d22aea879a7c6a7dbac31
Karl Cardenas <karl@spectrocloud.com>
2024-03-29 15:56:58 -0700

    chore: fixed logic (#2529)

--- src/components/ReleaseNotesVersions/ReleaseNotesVersions.tsx
+++ src/components/ReleaseNotesVersions/ReleaseNotesVersions.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5497095cced0073aa0b5b3a8d49a9f72cde6653e27bab74a3c38eec0fd1016ee
-size 5123
+oid sha256:8015f4c8e979af3fa2ffc3d588d16a9ca957ead6feff06bb40c2fa2ac927b25e
+size 5122
