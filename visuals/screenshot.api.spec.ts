4806e28d36c42751ad562ac820f0af997ab1cd53
Karl Cardenas <karl@spectrocloud.com>
2024-04-02 16:48:11 -0700

    ci: fix branch determination logic (#2568)
    * ci: fix branch determination logic
    
    * chore: fix eslint
    
    * ci: fix label logic
    
    * chore: revert logic that was working
    
    * chore: nope - src  filtering clash
    
    * chore: new changes
    
    * ci: verify variables
    
    * ci: fix
    
    * ci: path variable
    
    * ci: final change

--- visuals/screenshot.api.spec.ts
+++ visuals/screenshot.api.spec.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:398381a894159a98438d8550cdd03783e859f0b72b553898ec274c03db6df1be
-size 1677
+oid sha256:29c3c4713d33afc0da5790ddaffe3b66289c5c6a4d6a6657b5ba5a2a2639bf56
+size 1666
--- visuals/screenshot.docs.spec.ts
+++ visuals/screenshot.docs.spec.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65763843b4e6eb7d7da6c9b4e8990f3b0d46937bb7d3746c2fa379fb7bb7f5cf
-size 1681
+oid sha256:236ec55cc158bbc294f6c122c1168000202ab95e473cb9c288349c8abffe3008
+size 1657
