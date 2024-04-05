f5d290e2385024f3fe64e2f55d6842eeb2cf176f
Adelina Simion <43963729+addetz@users.noreply.github.com>
2024-04-05 19:58:47 +0200

    docs: upgrade to docusaurus 3.2.1 DOC-1134 (#2589)
    This patch makess the following changes:
    - Upgrades Docusaurus to 3.2.1
    - Fixes breaking changes with `contentLoaded` function
    - Upgrades `docusaurus-plugin-openapi-docs` and `docusaurus-theme-openapi-docs` to the newest beta

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bad3c92c272868fe0ab1bab4c0d92dc5b265866778503149caaa6491b2be18af
-size 2613
+oid sha256:a8a2073a28b633ec6c6f40d3da9573757f134853b7ebc0f86c44680a1455db56
+size 2616
--- plugins/packs-integrations.js
+++ plugins/packs-integrations.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03d581bb9f412574604b508b7a98bfff16a21268a131e75fd469799b1cab3c6e
-size 1159
+oid sha256:adc82435e4bd7bbba1f79eceab0fafe4ea10271de27174c786e953cfd0dca5ae
+size 1162
