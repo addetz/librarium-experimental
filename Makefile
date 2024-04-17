fe0c6182314d8ce6e60af9cfc2bc56fc667bfc03
Karl Cardenas <karl@spectrocloud.com>
2024-04-17 14:56:25 -0700

    chore: added dev aloglia config (#2654)
    * chore: added dev aloglia config
    
    * chore: add trailing slash
    
    * save
    
    * chore: updated aloglia index
    
    * chore: test
    
    * chore: test
    
    * chore: update config
    
    * chore: reverted index name
    
    * chore: gitleaks fix

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cef952831cb326a36e030580066896ed502c29e20f6b5a1c75495fa1abd80c87
-size 7604
+oid sha256:1953decdce76f180755e566a987afe9e7196ecdeb98d49afed6930b2fb6877f0
+size 8154
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f19cb96f209d6256e72bea16fa8dd3ecfcbeead7f2007d2dfd1e33117915aa8
-size 13324
+oid sha256:f7d0c83306fcf8484a4b1571631b65bd62d92541a34b1ea695c9466ce530b3c0
+size 13354
