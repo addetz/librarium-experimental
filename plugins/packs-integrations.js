600a4d37cd214eee28351d1858fb7f89fb8de173
Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
2024-10-16 14:30:17 -0700

    chore: fix bug in packs plugin (#4293)

--- plugins/packs-integrations.js
+++ plugins/packs-integrations.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6480b09e191d48b567413d06b282442e22ef2c2d382d7becd0b6bff1af93186b
-size 20382
+oid sha256:8fb6e2c4966ee0cfa382f86b7929054ed2b8c6156922019d6f81f42ad0935026
+size 20812
