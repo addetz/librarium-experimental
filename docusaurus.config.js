aebe2798e71d917a538ea40997d288e4d156a674
Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
2024-10-19 08:25:45 -0700

    ci: Robots Txt Fix (#4380)
    * chore: fix docusaurus format
    
    * ci: fix robots workflow
    
    * ci: update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c6622081371310c7cf17ca278ee1afc3b761c715f217b1b2deb88cd80620032
-size 13123
+oid sha256:13224931b5ac52b8e720d7a844b1cae9bdb43fa26d4d8b37fc0ef77cedf00c81
+size 13105
